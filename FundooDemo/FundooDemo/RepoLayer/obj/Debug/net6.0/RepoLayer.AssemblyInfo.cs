4280f40c27761c577aa1a13ecc0721a57033740a
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-06-27 17:44:26 +0530

    [sakshi]added:rabbitmq implemented

--- FundooDemo/FundooDemo/RepoLayer/obj/Debug/net6.0/RepoLayer.AssemblyInfo.cs
+++ FundooDemo/FundooDemo/RepoLayer/obj/Debug/net6.0/RepoLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepoLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39200aa054ba7f31a8002cd63c323f342223aa99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24152c54a1365f26633224b8492757ebc569c32c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepoLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepoLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
