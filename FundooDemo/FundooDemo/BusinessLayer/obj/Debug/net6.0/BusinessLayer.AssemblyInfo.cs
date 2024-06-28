af0fa907d047e333f1fd16027166db6103054d51
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-06-28 15:48:07 +0530

    [sakshi]added : rabbit mq in user controller implemented

--- FundooDemo/FundooDemo/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ FundooDemo/FundooDemo/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24152c54a1365f26633224b8492757ebc569c32c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4280f40c27761c577aa1a13ecc0721a57033740a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
